bc6ba632625507b01187dbe92d38e58a7111ca6e
Sam-Umbra <samuuel2008@hotmail.com>
2025-04-04 10:10:37 -0300

    API Requests with JavaScript - Fase 1

--- Site/src/script.js
+++ Site/src/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const library = document.querySelector('.library');
+    /*library.innerHTML = null;*/
+    fetchApi(`books`);
+
     const select = document.getElementById('parameters');
     let selectParameter = select.value;
     select.addEventListener('change', (event) => {
         selectParameter = event.target.value;
     })
-
     const searchBar = document.getElementById('searchBar');
     searchBar.addEventListener('keydown', (event) => {
 
@@ -13,14 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
 
             const searchInput = searchBar.value;
-            const library = document.querySelector('.library');
 
             library.innerHTML = null;
 
             if (searchInput !== '') {
-                fetchApi(`http://localhost:8080/library/${selectParameter}/${searchInput}`);
+                fetchApi(`${selectParameter}/${searchInput}`);
             } else {
                 /*Construtor Padrão*/
+                fetchApi('books');
             }
         }
     })
@@ -28,12 +31,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnCircles = document.querySelectorAll('.btn-circle');
     btnCircles.forEach(element => {
         element.addEventListener('click', () => {
+
             if (element.classList.contains('selected-circle')) {
 
                 element.classList.remove('selected-circle');
 
+                library.innerHTML = null;
+                fetchApi('books');
+
             } else {
-                element.classList.add('selected-circle');   
+                btnCircles.forEach(circle => {
+                    circle.classList.remove('selected-circle');
+                })
+
+                let param1 = element.getAttribute('data-param-1');
+                let param2 = element.getAttribute('data-param-2');
+
+                console.log(param1, param2);
+                element.classList.add('selected-circle');
+
+                library.innerHTML = null;
+                fetchApi(`price?firstPrice=${param1}&secondPrice=${param2}`);
             }
         })
     })
@@ -41,18 +59,57 @@ document.addEventListener('DOMContentLoaded', () => {
     const stars = document.querySelectorAll('.fa-star');
     stars.forEach(element => {
         element.addEventListener('click', () => {
+            
             const value = parseInt(element.getAttribute('data-value'));
             starValue(value);
+            
         })
     })
 
+    const priceFields = document.querySelectorAll('.price-field');
+    priceFields.forEach(element => {
+
+        element.addEventListener('keydown', (event) => {
+
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                
+                library.innerHTML = null;
+                
+                betweenPrices();
+            }
+
+        })
+    })
+
+    const categories = document.querySelectorAll('.category');
+    categories.forEach(element => {
+        let category = element.innerHTML;
+        element.addEventListener('click', () => {
+
+            if (element.classList.contains('selected-category')) {
+                element.classList.remove('selected-category');
+                
+                library.innerHTML = null;
+                fetchApi('books');
+            } else {
+                categories.forEach(c => {
+                    c.classList.remove('selected-category');
+                })
+                element.classList.add('selected-category');
+
+                library.innerHTML = null;
+                fetchApi(`category/${category}`)
+            }
+        })
+    })
     
 })
 
-async function fetchApi(urn) {
+async function fetchApi(uri) {
     try {
         
-        const response = await fetch(urn);
+        const response = await fetch(`http://localhost:8080/library/${uri}`);
 
         if (!response.ok) {
             throw new Error("Error upon fetching the resource");
@@ -67,40 +124,128 @@ async function fetchApi(urn) {
 }
 
 function starValue(value) {
+
+    const star1 = document.getElementById('star-1');
+    const star2 = document.getElementById('star-2');
+    const star3 = document.getElementById('star-3');
+    const star4 = document.getElementById('star-4');
+    const star5 = document.getElementById('star-5');
+
     switch (value) {
         case 1:
-            console.log(value);
-            const star1 = document.getElementById('star-1');
             if (star1.classList.contains('selected-star')) {
                 star1.classList.remove('selected-star');
+                star2.classList.remove('selected-star');
+                star3.classList.remove('selected-star');
+                star4.classList.remove('selected-star');
+                star5.classList.remove('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi('books');
             } else {
                 star1.classList.add('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi(`evaluation/${value}`);
             }
             break;
     
         case 2:
-            console.log(value);
-            const star2 = document.getElementById('star-2');
             if (star2.classList.contains('selected-star')) {
+                star1.classList.remove('selected-star');
                 star2.classList.remove('selected-star');
+                star3.classList.remove('selected-star');
+                star4.classList.remove('selected-star');
+                star5.classList.remove('selected-star');
+
+                library.innerHTML = null;
+                fetchApi('books');
             } else {
+                star1.classList.add('selected-star');
                 star2.classList.add('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi(`evaluation/${value}`);
             }
             break;
 
         case 3:
-            console.log(value);
+            if (star3.classList.contains('selected-star')) {
+                star1.classList.remove('selected-star');
+                star2.classList.remove('selected-star');
+                star3.classList.remove('selected-star');
+                star4.classList.remove('selected-star');
+                star5.classList.remove('selected-star');
+
+                library.innerHTML = null;
+                fetchApi('books');
+            } else {
+                star1.classList.add('selected-star');
+                star2.classList.add('selected-star');
+                star3.classList.add('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi(`evaluation/${value}`);
+            }
             break;
 
         case 4:
-            console.log(value);
+            if (star4.classList.contains('selected-star')) {
+                star1.classList.remove('selected-star');
+                star2.classList.remove('selected-star');
+                star3.classList.remove('selected-star');
+                star4.classList.remove('selected-star');
+                star5.classList.remove('selected-star');
+
+                library.innerHTML = null;
+                fetchApi('books');
+            } else {
+                star1.classList.add('selected-star');
+                star2.classList.add('selected-star');
+                star3.classList.add('selected-star');
+                star4.classList.add('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi(`evaluation/${value}`);
+            }
             break;
 
         case 5:
-            console.log(value);
+            if (star5.classList.contains('selected-star')) {
+                star1.classList.remove('selected-star');
+                star2.classList.remove('selected-star');
+                star3.classList.remove('selected-star');
+                star4.classList.remove('selected-star');
+                star5.classList.remove('selected-star');
+
+                library.innerHTML = null;
+                fetchApi('books');
+            } else {
+                star1.classList.add('selected-star');
+                star2.classList.add('selected-star');
+                star3.classList.add('selected-star');
+                star4.classList.add('selected-star');
+                star5.classList.add('selected-star');
+                
+                library.innerHTML = null;
+                fetchApi(`evaluation/${value}`);
+            }
             break;
 
         default:
             break;
+    }
+}
+
+function betweenPrices() {
+    let minPrice = document.getElementById('min-price').value;
+    let maxPrice = document.getElementById('max-price').value;
+
+    if (minPrice !== '' && maxPrice !== '') {
+        fetchApi(`price?firstPrice=${minPrice}&secondPrice=${maxPrice}`);
+    } else if (minPrice === '' && maxPrice === ''){
+        fetchApi('books');
+    } else {
+        return;
     }
 }
